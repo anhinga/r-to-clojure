a8a68bada070db9683da769e082e94d29c927ef2
Jon Anthony <jsa.aerial@gmail.com>
2012-01-10 12:56:59 -0500

    Second generation hack using Java arrays for scope containers

--- R-be.clj
+++ R-be.clj
@@ -113,17 +113,174 @@
 ;;; java array as a seq and then moves over that. UGH!
 ;;;
 (defmacro Rfor [i exp & body]
-    `(doseq [~i ~exp]
-       ~@body)))
+  (let [convert-forms ['Rdouble-array 'double-array int-array float-array]]
+    (cond
+     (and (sexp? exp) (in (first exp) convert-forms))
+     (let [stop (nth exp (if (= (first exp) 'Rdouble-array) 2 1))]
+       `(dotimes [~i (int ~stop)]
+          ~@body))
+     ;; Needs expansion on various other possible R cases...
+     :else
+     `(doseq [~i ~exp]
+        ~@body))))
 
 
-;;; The intent here is that this could be a _stacking_ scope
-;;; mechanism.  Currently it does NOT do that, but easily could be
-;;; changed to do that.
+
+(defmacro float+ [& args]
+  `(double (+ ~@(map #(do `(double ~%)) args))))
+
+(defmacro float- [& args]
+  `(double (- ~@(map #(do `(double ~%)) args))))
+
+(defmacro float* [& args]
+  `(double (* ~@(map #(do `(double ~%)) args))))
+
+(defmacro float-div [& args]
+  `(double (/ ~@(map #(do `(double ~%)) args))))
+
+
+(defn Rtype [x]
+  (cond
+   ;; Arrays
+   (sexp? x)
+   (let [op (first x)]
+     (cond
+      (in op ['Rdouble-array 'double-array 'float-array 'int-array
+              'char-array 'string-array 'make-array])
+      (let [tsym (first x)]
+        `(:array
+          ~(cond
+            (in tsym ['Rdouble-array 'double-array 'float-array]) :double
+            (= tsym 'int-array) :int
+            (= tsym 'char-array) :char
+            (= tsym 'string-array) :string
+            :else :object)))
+
+      (in op ['float* 'float+ 'float- 'float-div])
+      :double
+
+      (= op 'function)
+      :function
+
+      :else ; WTF is this?
+      'Object))
+
+   (float? x)
+   :double
+
+   (integer? x)
+   :int
+
+   (char? x)
+   :char
+
+   (string? x)
+   :string
+
+   :else ; WTF is this???
+   'Object))
+
+
+(defn Rarray-type? [x]
+  (and (sexp? x) (= (first x) :array)))
+
+(defn Rfunction-type? [x]
+  (= x :function))
+
+
+
+
+;;; This is a _stacking_ scope mechanism.  For the current hack, each
+;;; scope maintains
+;;;
+;;; 1) a set of scope vars (which will become the runtime scope
+;;; containers after processing in Rcode-gen) - one each for floats
+;;; (actually doubles), ints, char, and strings.  Scope "containers"
+;;; are Java arrays (mutable...)  of one of the types double, int,
+;;; char, or string.  NOTE: arrays (or other "objects") can just be
+;;; implemented with standard Clojure bindings as the arrays are not
+;;; assigned - only their _contents_ updated.
+;;;
+;;; 2) An ordinal count of all the vars for this scope.  This is used
+;;; to provide var "binding" ordering and also to provide vars with
+;;; their index into their appropriate scope containers
+;;;
+;;; 3) A map of var packets associated by the var symbol name.  Each
+;;; packet contains at least the vars index and type.  These are used
+;;; to guide code generation for accessing the var.
 ;;;
 ;;; Bound afresh for each call to Rcode-gen
 ;;;
-(def *scopes* :vars)
+(def *scopes* (atom :vars))
+
+(defn var-info [vars ord]
+  (if (empty? vars)
+    [0 nil nil]
+    (let [cnt (count vars)
+          [vars types] (apply map vector vars)
+          ords (take cnt (iterate inc ord))
+          infos (map #(do {:index %1 :type %2}) ords types)]
+      [cnt vars infos])))
+
+
+(defn push-scope [& args]
+  (let [[cnt vars infos] (var-info args 0)
+        vars (if (empty? args) {}
+                 (apply hash-map (interleave vars infos)))
+        types [:double :int :char :string :float]
+        svars (map #(gensym (str (name %) "-vars-")) types)
+        scope-vars (apply hash-map (interleave types svars))]
+    (swap! *scopes*
+           #(cons {:scope scope-vars
+                   :ordinal cnt
+                   :vars vars}
+                  %))))
+
+(defn pop-scope []
+  (swap! *scopes* #(rest %)))
+
+(defn add-vars [& vars]
+  (let [scope (first @*scopes*)
+        [cnt vars infos] (var-info vars (scope :ordinal))
+        ordinal (+ (scope :ordinal) cnt)
+        vars (apply assoc (scope :vars) (interleave vars infos))]
+    (swap! *scopes* #(cons (assoc (first %) :vars vars :ordinal ordinal)
+                           (rest %)))))
+
+(defn get-var-scope [x & {info :info :or {info false}}]
+  (let [scope (some #(when ((get % :vars) x) %) @*scopes*)]
+    (when scope
+      (if info
+        scope
+        (let [vtype (((scope :vars) x) :type)]
+          (if (Rarray-type? vtype)
+            ;; Arrays are Clj vars bound in enclosing scope (no
+            ;; "containers"), so the "scope var" is just x!
+            x
+            ;; Scalars are container map indexed by var x's type
+            ((scope :scope) vtype)))))))
+
+(defn add-var-initval [x initval]
+  (let [info (((get-var-scope x :info true) :vars) x)]
+    (swap! *scopes* #(cons (assoc-in (first %1) [:vars x :initval] initval)
+                           (rest %1)))))
+
+(defn get-var-index [x]
+  (let [m (get-var-scope x :info true)]
+    (when m (((m :vars) x) :index))))
+
+(defn get-var-type [x]
+  (let [m (get-var-scope x :info true)]
+    (when m (((m :vars) x) :type))))
+
+(defmacro Rset [x val]
+  `(aset ~(get-var-scope x) ~(get-var-index x) ~val)))
+
+(defmacro Rget [x]
+  (if (Rarray-type? (get-var-type x))
+    ;; Arrays are regular Clj vars
+    x
+    `(aget ~(get-var-scope x) ~(get-var-index x))))
 
 
 ;;; This has mutated to be more than just a cleanup pass.  Which means
@@ -151,21 +308,30 @@
 
          ;; Else, here we collect all mutated vars for later outer
          ;; scope creation (in driver).  If this var has already been
-         ;; collected, insert an assignment for it (var-set ...)
+         ;; collected, insert an assignment for it (Rset ...)
          ;;
          (let [[v exp] (take 2 (drop 1 body))
                v (cleaner v :lhs true)
-               exp (cleaner exp)]
+               exp (cleaner exp)
+               exp-type (Rtype exp)]
            (if (and (sexp? v) (= (first v) 'aget))
              ;; An array var, use aset instead
              `(aset ~(nth  v 1) ~(nth v 2) ~exp)
              ;; Else, collect to scope vars and issue assignment for it
              (let [body (drop 3 body) ; move to next exps in this body
-                   setform `(var-set ~v ~exp)]
-               (when-not (get @*scopes* v)
-                 (swap! *scopes* #(assoc %1 %2 [(gen-uid) exp]) v))
-               (if (seq body)
-                 (cons setform (keep cleaner body))
+                   _ (when-not (get-var-scope v) (add-vars [v exp-type]))
+                   setform (macroexpand-1 `(Rset ~v ~exp))]
+               (cond
+                (Rarray-type? exp-type)
+                (do (add-var-initval v exp) nil)
+
+                (Rfunction-type? exp-type)
+                (list v exp)
+
+                (seq body)
+                (cons setform (keep cleaner body))
+
+                :else
                  setform)))))
 
        :call-closure
@@ -188,7 +354,7 @@
      (case k
        :symbol
        (let [x (read-string x)]
-         (if (and (get @*scopes* x) (not lhs)) `(var-get ~x) x))
+         (if (and (get-var-scope x) (not lhs)) (macroexpand-1 `(Rget ~x)) x))
 
        :op ; these things really should be like :special-form, et.al.
        (case x
@@ -197,6 +363,10 @@
          "for" 'Rfor
          "[" 'aget ; Could be used in FOR loop index var analysis
          ":" 'Rdouble-array ; Broken, Needs :f in FOR sets
+         "*" 'float*
+         "+" 'float+
+         "-" 'float-
+         "/" 'float-div
          ;; A lot of stuff "subsummed" here that must be dealt with.
          (read-string x))
 
@@ -211,28 +381,106 @@
 
 
 (defn Rcode-gen [sexp]
-  (binding [*scopes* (atom {})]
+  (binding [*scopes* (atom ())]
+    (push-scope)
     (let [raw (cleaner sexp)
-          func-name (nth raw 1)
-          func-forms (nth raw 2)
+          func-name (nth raw 0)
+          func-forms (nth raw 1)
           func-params (vec (rest (second func-forms)))
           func-body (rest (nth func-forms 2))
-          nv-map (reduce (fn[m [k v]] (assoc m k v)) {}
-                         (map (fn[[k [o v]]] [k v])
-                              (sort-by (fn[[k v]] (first v))
-                                       (dissoc @*scopes* func-name))))
-          nv-vec (vec (interleave (keys nv-map) (vals nv-map)))
-          func-body `(with-local-vars ~nv-vec
+
+          scope (first @*scopes*)
+          cnt (scope :ordinal)
+          vars (scope :vars)
+          vtypes (reduce (fn[m [k v]]
+                           (let [vtype (v :type)]
+                             (if (or (Rarray-type? vtype)
+                                     (Rfunction-type? vtype))
+                               m
+                               (assoc m (v :type) true)))) {} vars)
+          container-nvs (keep (fn[[k v]]
+                                (when (vtypes k)
+                                  (let [a (case k
+                                            :double `(double-array ~cnt 0.0)
+                                            :int (`int-array ~cnt 0)
+                                            :float `(float-array ~cnt 0.0)
+                                            :char `(char-array ~cnt \space)
+                                            :string `(make-array String ~cnt))]
+                                    [v a])))
+                              (scope :scope))
+          array-nvs (keep (fn[[k v]]
+                            (when (Rarray-type? (v :type)) [k (v :initval)]))
+                          vars)
+          [ns vs] (apply map vector (concat container-nvs array-nvs))
+          nv-vec (vec (interleave ns vs))
+
+          func-body `(let ~nv-vec
                        ~@func-body)]
       `(defn ~func-name ~func-params ~func-body))))
+
+
+(defn Rcompile [x]
+  (clojure.lang.Compiler/eval (Rcode-gen x)))
 
 
 
 
 (pp/pprint (Rcode-gen *R-poly*))
 
-;;; Yields the following (though this is manually cleaned up wrt code
-;;; alignment and explicit clojure.core name space prefixes removed
+;;; This is what is in *scopes*:
+({:scope {:float float-vars-21822, :string string-vars-21821, :char char-vars-21820, :int int-vars-21819, :double double-vars-21818}, :ordinal 7, :vars {trpol2 {:index 6, :type :function}, s {:index 5, :type :double}, tm1 {:index 4, :type :double}, tp1 {:index 3, :type :double}, pol {:initval (Rdouble-array 1.0 100.0), :index 2, :type (:array :double)}, pu {:index 1, :type :double}, mu {:index 0, :type :double}}})
+
+;;; This is slightly prettied up from the pprint output
+;;;
+(defn trpol2 [n x]
+  ;; All _scalar_ variables in trpol2 are doubles, so we only have one
+  ;; container for them.  POL was an array anyway, so we just use it
+  ;; with its initial value.
+  (let [double-vars-21687 (double-array 7 0.0)
+        pol (Rdouble-array 1.0 100.0)]
+    (aset double-vars-21687 0 10.0)                ; mu
+    (aset double-vars-21687 1 0.0)                 ; pu
+    (aset double-vars-21687 3 2.0)                 ; tp1
+    (aset double-vars-21687 4 (float-div 1.0 2.0)) ; tm1
+    ;; All the float... macros rewrite the expression with (float ...)
+    ;; coercions everywhere and for the result as well.  This gives
+    ;; the Clojure compiler full automatic type hints for scalar
+    ;; arithmetic.  Integers would be dealt with similarly.  Mixtures
+    ;; would be promoted to double arithmetic.
+    (Rfor i (Rdouble-array 1.0 n)
+      (do (Rfor j (Rdouble-array 1.0 100.0)
+            (do (aset double-vars-21687 0
+                      (float* (float+ (aget double-vars-21687 0)
+                                      (aget double-vars-21687 3))
+                              (aget double-vars-21687 4)))
+                (aset pol j (aget double-vars-21687 0))))
+          (aset double-vars-21687 5 0.0)           ; s
+          (Rfor j (Rdouble-array 1.0 100.0)
+                (do (aset double-vars-21687 5
+                          (float+ (float* x (aget double-vars-21687 5))
+                                  (aget pol j)))))
+          (aset double-vars-21687 1 (float+ (aget double-vars-21687 5)
+                                            (aget double-vars-21687 1)))))
+    (print (aget double-vars-21687 1))))
+
+
+;;; A run...
+edu.bc.bio.R-be> (Rcompile *R-poly*)
+#'edu.bc.bio.R-be/trpol2
+edu.bc.bio.R-be> (time (trpol2 5000 0.2))
+12500.0"Elapsed time: 9.14068 msecs"
+nil
+edu.bc.bio.R-be> (time (trpol2 500000 0.2))
+1250000.0"Elapsed time: 902.936831 msecs"
+nil
+
+
+
+
+;;; The following is OBSOLETE.  It was the output from the original
+;;; hack.  Yields the following (though this is manually cleaned up
+;;; wrt code alignment and explicit clojure.core name space prefixes
+;;; removed
 ;;;
 (defn trpol2 [n x]
   (with-local-vars
